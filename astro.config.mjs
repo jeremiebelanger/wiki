95152537ac0b1c3ba602161e1eba5c6898b67f96
xalixilax <xalixilax@gmail.com>
2024-01-25 17:46:55 -0500

    change: Mise en place de dossiers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,36 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  base: "/",
+  integrations: [
+    starlight({
+      title: "Equine Duty",
+      social: {
+        github: "https://github.com/Equine-Duty",
+      },
+      sidebar: [
+        {
+          label: "Bienvenue",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "", link: "/" },
+          ],
+        },
+        {
+          label: "API",
+          autogenerate: { directory: "api" },
+        },
+        {
+          label: "Client",
+          autogenerate: { directory: "client" },
+        },
+        {
+          label: "Admin",
+          autogenerate: { directory: "admin" },
+        },
+      ],
+    }),
+  ],
 });
